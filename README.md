1dbe92f3440f780302e333acb23f9864719bbb79
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-04-28 13:28:22 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# NodeJs Project
+## Project: Portfolio
+
+- NodeJs + Express + MySQL + PHPMyAdmin
+- Using Custome Theme Starter Bootstrap.
+- Using Multer for Image Uploads.
+- Adding Simple CRUD for new Projects.
---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+
+var routes = require('./routes/index');
+var admin = require('./routes/admin');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(flash());
+
+app.locals.moment = require('moment');
+app.use(function(req, res, next){
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value){
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+    }
+}));
+
+app.use('/', routes);
+app.use('/admin', admin);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
